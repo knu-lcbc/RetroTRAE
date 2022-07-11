9271d7cca7092fcc6bd061982cf444fe38162e8d
ashyrmamatov01 <67892025+ashyrmamatov01@users.noreply.github.com>
2022-07-11 18:04:06 +0900

    Update pubchem_gen.py

--- pubchem_gen.py
+++ pubchem_gen.py
@@ -19,7 +19,7 @@ if __name__ =='__main__':
     parser.add_argument('--lines-per-file', type=int, default=500000, help="In order to search effectively PubChem database needs to be split to smaller files")
     parser.add_argument('--num-sample', type=int, default=None, help="Number of lines of 'CID-SMILES' file  for quick experimenting")
     parser.add_argument('--rdLogger', action='store_true', default=False, help="Enable RDKit Logger. By default it ignores error messages from the RDKit")
-    parser.add_argument('--tmp-file', default='./data/PubChem_AEs.tpm', type=str, help="A file name for temporarily use")
+    parser.add_argument('--tmp-file', default='./data/PubChem_AEs.tmp', type=str, help="A file name for temporarily use")
     args = parser.parse_args()
 
     logger.info(args)
